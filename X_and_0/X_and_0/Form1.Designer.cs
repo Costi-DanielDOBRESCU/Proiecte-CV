504d90e27f4de83f7cd23da35c763a76e7b4b31f
Costi-DanielDOBRESCU <73436601+Costi-DanielDOBRESCU@users.noreply.github.com>
2022-12-23 12:54:46 +0200

    X si 0 #1

---
+++ X_and_0/X_and_0/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace X_and_0
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScor = new System.Windows.Forms.Label();
+            this.btn1x1 = new System.Windows.Forms.Button();
+            this.btn1x2 = new System.Windows.Forms.Button();
+            this.btn1x3 = new System.Windows.Forms.Button();
+            this.btn2x3 = new System.Windows.Forms.Button();
+            this.btn2x2 = new System.Windows.Forms.Button();
+            this.btn2x1 = new System.Windows.Forms.Button();
+            this.btn3x3 = new System.Windows.Forms.Button();
+            this.btn3x2 = new System.Windows.Forms.Button();
+            this.btn3x1 = new System.Windows.Forms.Button();
+            this.lblRand = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScor
+            // 
+            this.lblScor.AutoSize = true;
+            this.lblScor.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScor.Location = new System.Drawing.Point(112, 18);
+            this.lblScor.Name = "lblScor";
+            this.lblScor.Size = new System.Drawing.Size(144, 32);
+            this.lblScor.TabIndex = 0;
+            this.lblScor.Text = "Scor 0 : 0";
+            // 
+            // btn1x1
+            // 
+            this.btn1x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1x1.Location = new System.Drawing.Point(28, 67);
+            this.btn1x1.Name = "btn1x1";
+            this.btn1x1.Size = new System.Drawing.Size(100, 100);
+            this.btn1x1.TabIndex = 1;
+            this.btn1x1.UseVisualStyleBackColor = true;
+            this.btn1x1.Click += new System.EventHandler(this.btn1x1_Click);
+            // 
+            // btn1x2
+            // 
+            this.btn1x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1x2.Location = new System.Drawing.Point(134, 67);
+            this.btn1x2.Name = "btn1x2";
+            this.btn1x2.Size = new System.Drawing.Size(100, 100);
+            this.btn1x2.TabIndex = 2;
+            this.btn1x2.UseVisualStyleBackColor = true;
+            this.btn1x2.Click += new System.EventHandler(this.btn1x2_Click);
+            // 
+            // btn1x3
+            // 
+            this.btn1x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1x3.Location = new System.Drawing.Point(240, 67);
+            this.btn1x3.Name = "btn1x3";
+            this.btn1x3.Size = new System.Drawing.Size(100, 100);
+            this.btn1x3.TabIndex = 3;
+            this.btn1x3.UseVisualStyleBackColor = true;
+            this.btn1x3.Click += new System.EventHandler(this.btn1x3_Click);
+            // 
+            // btn2x3
+            // 
+            this.btn2x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2x3.Location = new System.Drawing.Point(240, 173);
+            this.btn2x3.Name = "btn2x3";
+            this.btn2x3.Size = new System.Drawing.Size(100, 100);
+            this.btn2x3.TabIndex = 6;
+            this.btn2x3.UseVisualStyleBackColor = true;
+            this.btn2x3.Click += new System.EventHandler(this.btn2x3_Click);
+            // 
+            // btn2x2
+            // 
+            this.btn2x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2x2.Location = new System.Drawing.Point(134, 173);
+            this.btn2x2.Name = "btn2x2";
+            this.btn2x2.Size = new System.Drawing.Size(100, 100);
+            this.btn2x2.TabIndex = 5;
+            this.btn2x2.UseVisualStyleBackColor = true;
+            this.btn2x2.Click += new System.EventHandler(this.btn2x2_Click);
+            // 
+            // btn2x1
+            // 
+            this.btn2x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2x1.Location = new System.Drawing.Point(28, 173);
+            this.btn2x1.Name = "btn2x1";
+            this.btn2x1.Size = new System.Drawing.Size(100, 100);
+            this.btn2x1.TabIndex = 4;
+            this.btn2x1.UseVisualStyleBackColor = true;
+            this.btn2x1.Click += new System.EventHandler(this.btn2x1_Click);
+            // 
+            // btn3x3
+            // 
+            this.btn3x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3x3.Location = new System.Drawing.Point(240, 279);
+            this.btn3x3.Name = "btn3x3";
+            this.btn3x3.Size = new System.Drawing.Size(100, 100);
+            this.btn3x3.TabIndex = 9;
+            this.btn3x3.UseVisualStyleBackColor = true;
+            this.btn3x3.Click += new System.EventHandler(this.btn3x3_Click);
+            // 
+            // btn3x2
+            // 
+            this.btn3x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3x2.Location = new System.Drawing.Point(134, 279);
+            this.btn3x2.Name = "btn3x2";
+            this.btn3x2.Size = new System.Drawing.Size(100, 100);
+            this.btn3x2.TabIndex = 8;
+            this.btn3x2.UseVisualStyleBackColor = true;
+            this.btn3x2.Click += new System.EventHandler(this.btn3x2_Click);
+            // 
+            // btn3x1
+            // 
+            this.btn3x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3x1.Location = new System.Drawing.Point(28, 279);
+            this.btn3x1.Name = "btn3x1";
+            this.btn3x1.Size = new System.Drawing.Size(100, 100);
+            this.btn3x1.TabIndex = 7;
+            this.btn3x1.UseVisualStyleBackColor = true;
+            this.btn3x1.Click += new System.EventHandler(this.btn3x1_Click);
+            // 
+            // lblRand
+            // 
+            this.lblRand.AutoSize = true;
+            this.lblRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRand.Location = new System.Drawing.Point(94, 392);
+            this.lblRand.Name = "lblRand";
+            this.lblRand.Size = new System.Drawing.Size(181, 32);
+            this.lblRand.TabIndex = 10;
+            this.lblRand.Text = "Randul lui X";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 443);
+            this.Controls.Add(this.lblRand);
+            this.Controls.Add(this.btn3x3);
+            this.Controls.Add(this.btn3x2);
+            this.Controls.Add(this.btn3x1);
+            this.Controls.Add(this.btn2x3);
+            this.Controls.Add(this.btn2x2);
+            this.Controls.Add(this.btn2x1);
+            this.Controls.Add(this.btn1x3);
+            this.Controls.Add(this.btn1x2);
+            this.Controls.Add(this.btn1x1);
+            this.Controls.Add(this.lblScor);
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Joc X si 0";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScor;
+        private System.Windows.Forms.Button btn1x1;
+        private System.Windows.Forms.Button btn1x2;
+        private System.Windows.Forms.Button btn1x3;
+        private System.Windows.Forms.Button btn2x3;
+        private System.Windows.Forms.Button btn2x2;
+        private System.Windows.Forms.Button btn2x1;
+        private System.Windows.Forms.Button btn3x3;
+        private System.Windows.Forms.Button btn3x2;
+        private System.Windows.Forms.Button btn3x1;
+        private System.Windows.Forms.Label lblRand;
+    }
+}
+
---
+++ X_and_0/X_and_0/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_and_0
+{
+    public partial class mainForm : Form
+    {
+        public void Reset()
+        {
+            btn1x1.Text = "";
+            btn1x2.Text = "";
+            btn1x3.Text = "";
+            btn2x1.Text = "";
+            btn2x2.Text = "";
+            btn2x3.Text = "";
+            btn3x1.Text = "";
+            btn3x2.Text = "";
+            btn3x3.Text = "";
+
+            btn1x1.ResetBackColor();
+            btn1x2.ResetBackColor();
+            btn1x3.ResetBackColor();
+            btn2x1.ResetBackColor();
+            btn2x2.ResetBackColor();
+            btn2x3.ResetBackColor();
+            btn3x1.ResetBackColor();
+            btn3x2.ResetBackColor();
+            btn3x3.ResetBackColor();
+
+            btn1x1.Enabled = true;
+            btn1x2.Enabled = true;
+            btn1x3.Enabled = true;
+            btn2x1.Enabled = true;
+            btn2x2.Enabled = true;
+            btn2x3.Enabled = true;
+            btn3x1.Enabled = true;
+            btn3x2.Enabled = true;
+            btn3x3.Enabled = true;
+
+            btn1x1.TabStop = false;
+            btn1x2.TabStop = false;
+            btn1x3.TabStop = false;
+            btn2x1.TabStop = false;
+            btn2x2.TabStop = false;
+            btn2x3.TabStop = false;
+            btn3x1.TabStop = false;
+            btn3x2.TabStop = false;
+            btn3x3.TabStop = false;
+
+            lblRand.Text = "Randul lui X";
+        }
+        public void Castig()
+        {
+            if (btn1x1.Text == "X" && btn1x2.Text == "X" && btn1x3.Text == "X")
+            {
+                btn1x1.BackColor = Color.Blue;
+                btn1x2.BackColor = Color.Blue;
+                btn1x3.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x1.Text == "0" && btn1x2.Text == "0" && btn1x3.Text == "0")
+            {
+                btn1x1.BackColor = Color.Red;
+                btn1x2.BackColor = Color.Red;
+                btn1x3.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn2x1.Text == "X" && btn2x2.Text == "X" && btn2x3.Text == "X")
+            {
+                btn2x1.BackColor = Color.Blue;
+                btn2x2.BackColor = Color.Blue;
+                btn2x3.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn2x1.Text == "0" && btn2x2.Text == "0" && btn2x3.Text == "0")
+            {
+                btn2x1.BackColor = Color.Red;
+                btn2x2.BackColor = Color.Red;
+                btn2x3.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn3x1.Text == "X" && btn3x2.Text == "X" && btn3x3.Text == "X")
+            {
+                btn3x1.BackColor = Color.Blue;
+                btn3x2.BackColor = Color.Blue;
+                btn3x3.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn3x1.Text == "0" && btn3x2.Text == "0" && btn3x3.Text == "0")
+            {
+                btn3x1.BackColor = Color.Red;
+                btn3x2.BackColor = Color.Red;
+                btn3x3.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn1x1.Text == "X" && btn2x1.Text == "X" && btn3x1.Text == "X")
+            {
+                btn1x1.BackColor = Color.Blue;
+                btn2x1.BackColor = Color.Blue;
+                btn3x1.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x1.Text == "0" && btn2x1.Text == "0" && btn3x1.Text == "0")
+            {
+                btn1x1.BackColor = Color.Red;
+                btn2x1.BackColor = Color.Red;
+                btn3x1.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn1x2.Text == "X" && btn2x2.Text == "X" && btn3x2.Text == "X")
+            {
+                btn1x2.BackColor = Color.Blue;
+                btn2x2.BackColor = Color.Blue;
+                btn3x2.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn1x2.Text == "0" && btn2x2.Text == "0" && btn3x2.Text == "0")
+            {
+                btn1x2.BackColor = Color.Red;
+                btn2x2.BackColor = Color.Red;
+                btn3x2.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }
+            else if(btn1x3.Text == "X" && btn2x3.Text == "X" && btn3x3.Text == "X")
+            {
+                btn1x3.BackColor = Color.Blue;
+                btn2x3.BackColor = Color.Blue;
+                btn3x3.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x3.Text == "0" && btn2x3.Text == "0" && btn3x3.Text == "0")
+            {
+                btn1x3.BackColor = Color.Red;
+                btn2x3.BackColor = Color.Red;
+                btn3x3.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn1x1.Text == "X" && btn2x2.Text == "X" && btn3x3.Text == "X")
+            {
+                btn1x1.BackColor = Color.Blue;
+                btn2x2.BackColor = Color.Blue;
+                btn3x3.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x1.Text == "0" && btn2x2.Text == "0" && btn3x3.Text == "0")
+            {
+                btn1x1.BackColor = Color.Red;
+                btn2x2.BackColor = Color.Red;
+                btn3x3.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x3.Text == "X" && btn2x2.Text == "X" && btn3x1.Text == "X")
+            {
+                btn1x3.BackColor = Color.Blue;
+                btn2x2.BackColor = Color.Blue;
+                btn3x1.BackColor = Color.Blue;
+                MessageBox.Show("X a castigat !","X si 0",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                scorX++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if (btn1x3.Text == "0" && btn2x2.Text == "0" && btn3x1.Text == "0")
+            {
+                btn1x3.BackColor = Color.Red;
+                btn2x2.BackColor = Color.Red;
+                btn3x1.BackColor = Color.Red;
+                MessageBox.Show("0 a castigat !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                scor0++;
+                lblScor.Text = "Scor " + scorX + " : " + scor0;
+                Reset();
+            }else if(btn1x1.Text != "" && btn1x2.Text != "" && btn1x3.Text != ""
+                && btn2x1.Text != "" && btn2x2.Text != "" && btn2x3.Text != ""
+                && btn3x1.Text != "" && btn3x2.Text != "" && btn3x3.Text != "")
+            {
+                MessageBox.Show("Remiza !", "X si 0", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+            }
+        }
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        int scorX = 0, scor0 = 0;
+
+        private void btn1x1_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn1x1.Text = "X";
+                btn1x1.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }else if(lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn1x1.Text = "0";
+                btn1x1.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn1x2_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn1x2.Text = "X";
+                btn1x2.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn1x2.Text = "0";
+                btn1x2.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn1x3_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn1x3.Text = "X";
+                btn1x3.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn1x3.Text = "0";
+                btn1x3.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn2x1_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn2x1.Text = "X";
+                btn2x1.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn2x1.Text = "0";
+                btn2x1.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn2x2_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn2x2.Text = "X";
+                btn2x2.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn2x2.Text = "0";
+                btn2x2.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn2x3_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn2x3.Text = "X";
+                btn2x3.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn2x3.Text = "0";
+                btn2x3.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn3x1_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn3x1.Text = "X";
+                btn3x1.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn3x1.Text = "0";
+                btn3x1.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn3x2_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn3x2.Text = "X";
+                btn3x2.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn3x2.Text = "0";
+                btn3x2.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void btn3x3_Click(object sender, EventArgs e)
+        {
+            if (lblRand.Text.Equals("Randul lui X"))
+            {
+                btn3x3.Text = "X";
+                btn3x3.Enabled = false;
+                lblRand.Text = "Randul lui 0";
+            }
+            else if (lblRand.Text.Equals("Randul lui 0"))
+            {
+                btn3x3.Text = "0";
+                btn3x3.Enabled = false;
+                lblRand.Text = "Randul lui X";
+            }
+            Castig();
+        }
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
